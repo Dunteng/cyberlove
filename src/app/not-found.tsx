35ba2c24ad86e9525b8fc870df079b52d8a57e1e
daltonllin <daltonllin@tencent.com>
2025-02-08 20:21:30 +0800

    feat: 开发404页面

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,24 @@
+'use client'
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
-      <div className="text-center p-8 bg-white rounded-lg shadow-xl max-w-md mx-4">
-        <h2 className="text-6xl font-bold text-gray-800 mb-4">404</h2>
-        <h3 className="text-2xl font-semibold text-gray-700 mb-4">页面未找到</h3>
-        <p className="text-gray-600 mb-8">抱歉，您访问的页面似乎已经迷失在了网络的星辰大海中...</p>
-        <Link 
-          href="/" 
-          className="inline-block px-6 py-3 text-white font-semibold bg-gradient-to-r from-pink-500 to-purple-500 rounded-full hover:from-pink-600 hover:to-purple-600 transition-all duration-300 shadow-lg hover:shadow-xl"
-        >
-          返回首页
-        </Link>
-      </div>
+    <div className="min-h-screen flex items-center justify-center flex-col">
+      <DotLottieReact
+        src="https://lottie.host/b0a95dcd-e15a-4559-9399-e9383b138071/tCqL5RbsU1.lottie"
+        loop
+        autoplay
+      />
+      <h1 className="text-sm font-bold mb-4">你来到了没有人烟的荒原</h1>
+      <Link
+        href="/"
+        className="text-sm inline-block px-6 py-3 text-white font-semibold bg-black rounded-full shadow-lg hover:shadow-xl"
+      >
+        返回首页
+      </Link>
     </div>
   )
 }
+
